9955e8f23f68a82d9b53ba4d063d6942c165ac9a
LeoSatCode <LeoSatCode@users.noreply.github.com>
2025-02-11 17:13:19 +0000

    deploy: 05f303c754f32b8c66736c10f9873f85ce6378cd

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import '@angular/compiler';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
-  .catch(err => console.error(err));
